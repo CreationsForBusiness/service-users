511f836d149e12a516c6ac064f7d1a64efec37dd
Yeinso Blanco <yeinsojbm@gmail.com>
2021-02-23 18:18:59 -0500

    Fix migrate

--- migrate.js
+++ migrate.js
@@ -4,9 +4,6 @@ const { host = '', port = '', database = '', user = '', pass = '', prefix = '' }
 
 const authUri = !!user && !!pass ? `${user}:${pass}@` : '';
 const portUri = !!port && Number.isInteger(port) ? `:${port}` : '';
-
-  console.log("#########", port, portUri)
-
 const dbConnectionUri = `${prefix}://${authUri}${host}${portUri}/${database}`
 
 module.exports = {
