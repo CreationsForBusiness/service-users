d2ddb1eb11c6bc5c044ac80dbe181a1f450bbc0c
Yeinso Blanco <yeinsojbm@gmail.com>
2022-06-13 19:32:26 -0500

    Clean ip

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -6,7 +6,7 @@ module.exports = async (ctx, next) => {
   const { apps } = models;
   const appCode = ctx.get('app-code');
   const appId = ctx.get('app-id');
-  const ip = ctx.get('request-ip');
+  const ip = str.replace('::ffff:', ctx.get('request-ip'));
   const isValidRequest = validRequest.includes(url);
   const {
     valid, errorID, errorCode, message,
@@ -17,9 +17,7 @@ module.exports = async (ctx, next) => {
     ctx.throw(403, message);
   }
 
-  console.log(url, ip);
-
-  if (!isValidRequest && !(/^(?:[0-9]{1,3}\.){3}[0-9]{1,3}|::1|::ffff:127.0.0.1$/.test(ip))) {
+  if (!isValidRequest && !(/^(?:[0-9]{1,3}\.){3}[0-9]{1,3}|::1|127.0.0.1$/.test(ip))) {
     ctx.code = 'MAUT-1';
     ctx.throw(403, 'Invalid IP');
   }
