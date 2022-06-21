b16b73501a0893889f7b0171c200cb73e8f8f806
Yeinso Blanco <yeinsojbm@gmail.com>
2022-06-20 19:04:17 -0500

    fix migrations

--- migrate.js
+++ migrate.js
@@ -13,3 +13,4 @@ module.exports = {
   dbConnectionUri,
   'migrations-dir': './migrations',
 };
+
--- migrations/1611528353554-apps.js
+++ migrations/1611528353554-apps.js
@@ -5,10 +5,12 @@ const { getRandomString } = require('../src/libs/commons.lib');
 const envelope = 'CFB_001';
 const vetaux = 'CFB_002';
 
+
 /**
  * Make any changes you need to make to the database here
  */
 async function up () {
+  this('Apps', Apps);
   return Promise.all([
     this('Apps').create({ code: envelope, name: 'Envelopes', status: true, uuid: getRandomString(), shared: true, }),
     this('Apps').create({ code: vetaux, name: 'VetAux', status: true, uuid: getRandomString(), shared: false }),
@@ -19,6 +21,7 @@ async function up () {
  * Make any changes that UNDO the up function side effects here (if possible)
  */
 async function down () {
+  this('Apps', Apps);
   return Promise.all([
     this('Apps').deleteOne({ code: envelope }),
     this('Apps').deleteOne({ code: vetaux }),
